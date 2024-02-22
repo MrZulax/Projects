8bb805dd2a03abb2f39170a626705718d2568c0b
MrZulaxGitHub <zulaxsquad@yandex.kz>
2024-02-22 17:34:14 +0600

    Add

---
+++ iOS/Ios project/Ios project/Ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_projectApp.swift
+//  Ios project
+//
+//  Created by MrZulax on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
