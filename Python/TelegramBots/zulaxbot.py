fd5319ea8940bb42192064929c31d9cd9db55c9a
MrZulaxGitHub <zulaxsquad@yandex.kz>
2024-02-27 15:26:47 +0600

    Update zulaxbot.py

---
+++ Python/TelegramBots/zulaxbot.py
@@ -0,0 +1,7 @@
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        .... #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Запомню : )');
+    elif call.data == "no":
+         ... #переспрашиваем
